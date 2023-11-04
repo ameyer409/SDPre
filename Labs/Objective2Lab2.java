fd2f1b2ccb4868a2855ce12a6f50d258b461c63f
Allan Meyer <ameyer409@yahoo.com>
2023-11-04 11:40:45 -0400

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+//objective 2 lab 2: printing multiple variables
+
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Allan Meyer";
+		int age = 31;
+		String state = "Virginia";
+
+		System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+	}
+}

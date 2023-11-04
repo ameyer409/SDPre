41a2230fc43b0b60e374ce72b916cdbc559f9722
Allan Meyer <ameyer409@yahoo.com>
2023-11-04 18:25:43 -0400

    Complete Objective7Lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,12 @@
+// Objective 7 Lab 2 - Print 1 to 20
+
+public class Objective7Lab2 {
+	public static void main(String[] args) {
+		int counter = 0;
+
+		while(counter <=20) {
+			System.out.println(counter);
+			counter++;
+		}
+	}
+}

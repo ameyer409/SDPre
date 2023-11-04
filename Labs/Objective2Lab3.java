efda111ba12369820d0208b17ebe762a86ade36f
Allan Meyer <ameyer409@yahoo.com>
2023-11-04 11:46:06 -0400

    Complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+//Objective 2 Lab 3 - Declaring Variables
+
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int year = 1992;
+		String month = "February";
+		boolean weekend = false;
+
+		System.out.println(year);
+		System.out.println(month);
+		System.out.println(weekend);
+	}
+}

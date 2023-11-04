f0c6e995b6bf9199afcca5c80d9b4c34cf4a21a1
Allan Meyer <ameyer409@yahoo.com>
2023-11-04 18:22:38 -0400

    Complete Objective7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,14 @@
+// Objective 7 Lab 1 - Print Numbers in Reverse
+
+public class Objective7Lab1 {
+	public static void main(String[] args) {
+
+		int counter = 10;
+
+		while(counter > 0) {
+			System.out.println(counter);
+			counter--;
+		}
+	}
+
+}

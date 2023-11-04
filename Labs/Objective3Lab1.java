fdd16d9860ee3a0548cae4f54f068ae3611dfff4
Allan Meyer <ameyer409@yahoo.com>
2023-11-04 11:55:12 -0400

    Complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,19 @@
+//Objective 3 Lab 1 - Writing Expressions
+
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		//declare variables as doubles
+		double num1, num2, num3, average;
+
+		//assign values to use for calculating the average
+		num1 = 23.4;
+		num2 = 16.66;
+		num3 = 42.9;
+
+		//calculate the average and store in new variable average
+		average = (num1 + num2 + num3)/3;
+
+		//print the calculated average to the screen
+		System.out.println(average);
+	}
+}

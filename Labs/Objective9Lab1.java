67aaa24b9ea23559c48d1bf8116839db7eaead4a
Allan Meyer <ameyer409@yahoo.com>
2023-11-05 08:44:59 -0500

    Complete Objective 9 Labs 1 and 2

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,11 @@
+// Objective 9 Lab 1 - Print via Method
+
+public class Objective9Lab1 {
+	public static void main(String[] args) {
+		printName();
+	}
+
+	public static void printName() {
+		System.out.println("Hello Doug");
+	}
+}
---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,13 @@
+// Objective 9 Lab 2 - Print Message
+
+public class Objective9Lab2 {
+	public static void main (String[] args) {
+		System.out.println("In main");
+		printMessage();
+		System.out.println("In main");
+	}
+	public static void printMessage() {
+		System.out.println("In method");
+	}
+
+}

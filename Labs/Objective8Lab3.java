df65e09448af36a6495b3e7454c04e2e9c3791ee
Allan Meyer <ameyer409@yahoo.com>
2023-11-05 08:30:54 -0500

    Complete Objective8 Labs 1, 2 and 3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,15 @@
+// Objective 8 Lab 3 - Identify Odd and Even Numbers
+
+public class Objective8Lab3 {
+	public static void main(String[] args) {
+
+		for(int i = 1; i<=20; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i + " is even.");
+			}
+			else {
+				System.out.println(i + " is odd.");
+			}
+		}
+	}
+}
